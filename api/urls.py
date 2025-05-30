a4d3107a5ba921dd9567ea0dc57b1a8dadd92e92
Hudson O'Donnell <hudsonodonnell2004@gmail.com>
2025-05-30 10:57:21 -0400

    added docker file and documented in readme

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import CreateReceipt, RetrieveReceipt
 
+# Register the endpoints.
 urlpatterns = [
     path('receipts/process', CreateReceipt, name="create_receipt"),
     path('receipts/<uuid:pk>/points', RetrieveReceipt, name="create_receipt"),
